aff7894d75c38a49b25ab89095dc262aa5db0cfa
Yuanzhe <yzhdong@mob>
2017-05-16 23:09:41 -0700

    update

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask,render_template, request,json
 
 app = Flask(__name__)
 
+sents = ["Sys: Hello, welcom", "User: I want a gastropub food", "Sys: There're 4 restaurants serving Sichuan food, what price range do you want?"]
+
+sents2 = ["Sys: Hello, welcom", "User: I want a gastropub food", "Sys: There're 4 restaurants serving Sichuan food, what price range do you want?", "User: don't care"]
 @app.route('/')
 def hello():
     return 'Welcome to Python Flask!'
@@ -17,5 +20,35 @@ def signUpUser():
     password = request.form['password'];
     return json.dumps({'status':'OK','user':user,'pass':password});
 
+@app.route('/newUserTask')
+def newUserTask():
+    taskId = 123
+    foodType = "Sichuan food"
+    priceRange = "don't care"
+    address = "*"
+    lookingFor = "address"
+    global sents
+    return render_template('user.html', taskId=taskId, foodType=foodType, priceRange=priceRange, address=address, lookingFor=lookingFor, sents=sents)
+
+@app.route('/userUpdateTask', methods=['POST'])
+def userUpdateTask():
+    if request.method == "POST":
+        print request
+        content = request.get_json()
+        print content
+        taskId = content['taskId']
+        userResponse = content['userResponse']
+        print userResponse
+        global sents
+        sents.append("User: " + userResponse)
+        #print taskId
+        return json.dumps({'status':'OK','taskId': taskId, 'userResponse': userResponse})
+
+#Wizard
+@app.route('/newWizardTask')
+def newWizardTask():
+    taskId = 234
+    global sents2
+    return render_template('wizard.html', taskId=taskId, sents = sents2)
 if __name__=="__main__":
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,100 @@
-$(function(){
-	$('button').click(function(){
-		var user = $('#txtUsername').val();
-		var pass = $('#txtPassword').val();
-		$.ajax({
-			url: '/signUpUser',
-			data: $('form').serialize(),
-			type: 'POST',
-			success: function(response){
-				console.log(response);
-			},
-			error: function(error){
-				console.log(error);
-			}
-		});
-	});
+/*function submitUserResponse() {
+	console.log("test");
+	 console.log($('#taskId').val());
+    		var taskId = $('#taskId').val();
+    		var userResponse = $('#userResponse').val();
+    		var sendInfo = {
+
+                   };
+            console.log(JSON.stringify(sendInfo));
+    		$.ajax({
+    			url: '/userUpdateTask',
+    			contentType: "application/json; charset=utf-8",
+    			data: JSON.stringify(sendInfo),
+    			dataType: "json",
+    			type: 'POST',
+    			success: function(response){
+    				alert(response)
+    				console.log(response);
+    			},
+    			error: function(error){
+    				console.log(error);
+    			}
+    		});
+    	}
+    	*/
+ function submitUserResponse() {
+ 		var taskId = $('#taskId').text();
+ 		var userResponse = $('#userResponse').val();
+    	$.ajax({
+          type: 'POST',
+          url: "/userUpdateTask",
+          data: JSON.stringify({
+            "taskId": taskId,
+            "userResponse": userResponse,
+            "sampleSize" : 10
+          }),
+          error: function(e) {
+            console.log(e);
+          },
+          dataType: "json",
+          contentType: "application/json",
+          success: function(response){
+              				alert(JSON.stringify(response))
+              				console.log(response);
+              			}
+        });
+       };
+
+function searchDB() {
+ console.log("test")
+
+     // JSON Data
+var articles = [
+    {
+        "venue":"Cheng Du Xiao che",
+        "address":"Zhong guan cun",
+        "phone":["110"],
+        "tags":["jquery","json","$.each"]
+    },
+    {
+        "venue":"Sha Xian xiao chi",
+        "address": "da heng",
+    }
+];
+// 1. remove all existing rows
+$("tr:has(td)").remove();
+
+// 2. get each article
+$.each(articles, function (index, article) {
+
+    // 2.2 Create table column for categories
+    var td_categories = $("<td/>");
+
+    // 2.3 get each category of this article
+    /*$.each(article.categories, function (i, category) {
+        var span = $("<span/>");
+        span.text(category);
+        td_categories.append(span);
+    });*/
+
+    // 2.4 Create table column for tags
+   var td_tags = $("<td/>");
+
+    // 2.5 get each tag of this article
+    $.each(article.tags, function (i, tag) {
+        var span = $("<span/>");
+        span.text(tag);
+        td_tags.append(span);
+    });
+    var venueName = '<td> ' + article.venue + '</td>'
+    var address = '<td> ' + article.address + '</td>'
+	$('#added-articles').append( '<tr>' + venueName + address + '</tr>')
+    // 2.6 Create a new row and append 3 columns (title+url, categories, tags)
+    /*$("#added-articles").append($('<tr/>')
+            .append($('<td/>').html("<a href='"+article.url+"'>"+article.title+"</a>"))
+            .append(td_categories)
+            .append(td_tags)
+    );*/
 });
+}
